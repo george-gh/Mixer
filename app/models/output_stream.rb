05f7460594459fea5b920f743d1a1011c81b3686
george-gh <giorgio.bruschini@gmail.com>
2014-08-19 17:16:12 +0200

    New simple method for testing

--- app/models/output_stream.rb
+++ app/models/output_stream.rb
@@ -1,8 +1,21 @@
+# Class for output stream object
+# It needs almost a video stream
 class OutputStream
 
   include ActiveModel::Model
   extend ActiveModel::Naming
 
+  # Create object
+  def initialize _video = nil, _audio = nil, _overlay = nil
+    @video = _video
+    @audio = _audio
+    @overlay = _overlay
+  end
+
+  # Streaming url
+  def url
+    "http://localhost:8090/live.flv"
+  end
 
 
 end
