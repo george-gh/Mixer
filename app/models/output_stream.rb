356cb117af3d0f1143e8741093c2a9eef0860a86
george-gh <giorgio.bruschini@gmail.com>
2014-08-25 11:33:43 +0200

    FFmpeg e FFplay commands integrated in model class

--- app/models/output_stream.rb
+++ app/models/output_stream.rb
@@ -5,6 +5,9 @@ class OutputStream
   include ActiveModel::Model
   extend ActiveModel::Naming
 
+  FEED_NAME = "feed1.ffm"
+  OUTPUT_STREAM_NAME = "live.flv"
+
   # Create object
   def initialize _video = nil, _audio = nil, _overlay = nil
     @video = _video
@@ -17,5 +20,34 @@ class OutputStream
     "http://localhost:8090/live.flv"
   end
 
+  # Using ffmpeg command, create the ffm stream for ffserver
+  def create_ffm_stream
+    return false if @video.blank?
+    cmd = "ffmpeg -i #{@video.url} -vcodec copy http://localhost:8090/#{FEED_NAME} &"
+    spawn(cmd)
+  # rescue Exception => ex
+  #   return ex.message
+  end
+
+  # Launch ffplay command to stream output
+  def generate_stream
+    create_ffm_stream
+    # try to delete next row
+    sleep 3
+    cmd = "ffplay http://localhost:8090/#{OUTPUT_STREAM_NAME} &"
+    spawn(cmd)
+  # rescue Exception => ex
+  #   return ex.message
+  end
+
+  # Set overlay
+  def set_overlay
+    # TODO
+  end
+
+  # Set audio
+  def set_audio
+    # TODO
+  end
 
 end
